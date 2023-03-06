423b7cdb2fcaa8ec34b22f09989fe60371b44c6f
Egor Zaitsev <egor.zaitsev84@gmail.com>
2023-03-06 13:09:23 +0300

    release version

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let shiftedAlphabet = "";
 const key = document.getElementById('key');
 const textarea = document.getElementById("message");
 const button = document.getElementById("apply");
-const decrypted = document.getElementById("decrypted");
+const decrypted = document.getElementById("encrypted");
 
 let shifter = (n) => {
     for (let i = 0; i < alphabet.length; i++) {
@@ -25,15 +25,43 @@ let encode = (message) => {
         else result += " ";
         
     }
-
     decrypted.textContent = result;
 }
 
 
 
 button.onclick = function(){
-    console.log(textarea.value);
+    
     shifter(Number(key.value));
+    console.log(shiftedAlphabet);
     encode(textarea.value);
+    shiftedAlphabet= "";
+}
+
+const textDecrypt = document.getElementById("decrypt");
+const keyDecrypt = document.getElementById("keydecrypt");
+const btnDecrypt = document.getElementById("btnDecrypt");
+const resultDecrypt = document.getElementById("decrypted");
+
+let decrypt = (messageDecrypt) => {
+    messageDecrypt= messageDecrypt.toLowerCase();
+    let result = '';
+
+    for (let i = 0; i < messageDecrypt.length; i++) {
+        if(messageDecrypt[i]!==" ") {
+            let letter = shiftedAlphabet.indexOf(messageDecrypt[i]);
+            result += alphabet[letter];
+        }
+        else result += " ";
+    }
+
+    resultDecrypt.textContent = result;
+}
+
+
+btnDecrypt.onclick = function() {
+    shifter(Number(keyDecrypt.value));
+    console.log(shiftedAlphabet);
+    decrypt(textDecrypt.value);
     shiftedAlphabet= "";
 }
